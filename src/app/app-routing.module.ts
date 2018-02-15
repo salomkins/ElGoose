72e31f042d49f5b8f471a8442708ea33c2222472
Janis Salmins <salomkins@gmail.com>
2018-02-15 20:48:53 +0200

    Services module added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ import {RouterModule, Routes} from '@angular/router';
 
 const routes: Routes = [
   { path: 'home', loadChildren: 'app/pages/home/home.module#HomeModule' },
-  { path: 'about', loadChildren: 'app/pages/about/about.module#AboutModule' }
+  { path: 'about', loadChildren: 'app/pages/about/about.module#AboutModule' },
+  { path: 'services', loadChildren: 'app/pages/services/services.module#ServicesModule' }
 ];
 
 @NgModule({
