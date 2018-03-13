9eb762d22a45bbc8afd280d2c07f258223fef0db
Guntars V <v.guntars@inbox.lv>
2018-03-13 14:43:23 +0200

    Test1 procesā...

--- src/app/components/content-test/test-page-1/partners-list/partners-list.component.ts
+++ src/app/components/content-test/test-page-1/partners-list/partners-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-export class patnersList {
+export class partnersItem {
   countryCode: string;
   regNr: string;
   name: string;
@@ -11,17 +11,35 @@ export class patnersList {
   templateUrl: './partners-list.component.html',
   styleUrls: ['./partners-list.component.scss']
 })
+
 export class PartnersListComponent implements OnInit {
   listName = 'Partneru saraksts';
-  partner: patnersList = {
-    countryCode : 'LV',
-    regNr : '40123456789',
-    name : 'KUKU SIA'
-  }
+  myPartners = myPartnersList;
 
+  activeItem: partnersItem;
+  clickItem(item: partnersItem){
+    this.activeItem = item;
+  }
   constructor() { }
 
   ngOnInit() {
   }
-
 }
+
+var myPartnersList: partnersItem[] = [
+    {countryCode : 'LV',
+    regNr : '40123456789',
+    name : 'KUKU SIA'},
+    {countryCode : 'LV',
+    regNr : '40111111111',
+    name : 'AAA SIA'},
+    {countryCode : 'LV',
+    regNr : '40222222222',
+    name : 'BBB SIA'},
+    {countryCode : 'LV',
+    regNr : '40333333333',
+    name : 'CCC SIA'},
+    {countryCode : 'RU',
+    regNr : '40444444444',
+    name : 'DDD SIA'},
+];
--- src/app/components/content-test/test-page-1/test-page-1.module.ts
+++ src/app/components/content-test/test-page-1/test-page-1.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule }   from '@angular/forms';
 
 import { TestPage1RoutingModule } from './test-page-1-routing.module';
 import { TestPage1Component } from './test-page-1.component';
@@ -8,6 +9,7 @@ import { PartnersListComponent } from './partners-list/partners-list.component';
 @NgModule({
   imports: [
     CommonModule,
+    FormsModule,
     TestPage1RoutingModule
   ],
   declarations: [
