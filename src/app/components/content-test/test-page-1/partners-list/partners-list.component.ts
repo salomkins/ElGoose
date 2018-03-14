ce6958a154c572200531df97b1b5116fcc385ee5
Guntars V <v.guntars@inbox.lv>
2018-03-14 11:53:58 +0200

    Test1 procesā...

--- src/app/components/content-test/test-page-1/partners-list/partners-list.component.ts
+++ src/app/components/content-test/test-page-1/partners-list/partners-list.component.ts
@@ -17,9 +17,22 @@ export class PartnersListComponent implements OnInit {
   myPartners = myPartnersList;
 
   activeItem: partnersItem;
-  clickItem(item: partnersItem){
+  readActiveItem(item: partnersItem) {
     this.activeItem = item;
   }
+  clearActiveItem() {
+    this.activeItem = null;
+  }
+
+  editItem: partnersItem;
+  readEditItem(item: partnersItem) {
+    this.editItem = item;
+  }
+  clearEditItem() {
+    this.editItem = null;
+  }
+
+
   constructor() { }
 
   ngOnInit() {
@@ -27,19 +40,34 @@ export class PartnersListComponent implements OnInit {
 }
 
 var myPartnersList: partnersItem[] = [
-    {countryCode : 'LV',
-    regNr : '40123456789',
-    name : 'KUKU SIA'},
-    {countryCode : 'LV',
-    regNr : '40111111111',
-    name : 'AAA SIA'},
-    {countryCode : 'LV',
-    regNr : '40222222222',
-    name : 'BBB SIA'},
-    {countryCode : 'LV',
-    regNr : '40333333333',
-    name : 'CCC SIA'},
-    {countryCode : 'RU',
-    regNr : '40444444444',
-    name : 'DDD SIA'},
+  {
+    countryCode: 'LV',
+    regNr: '40123456789',
+    name: 'KUKU SIA'
+  },
+  {
+countryCode: 'LV',
+    regNr: '40111111111',
+    name: 'AAA SIA'
+  },
+  {
+    countryCode: 'LV',
+    regNr: '40222222222',
+    name: 'BBB SIA'
+  },
+  {
+    countryCode: 'LV',
+    regNr: '40333333333',
+    name: 'CCC SIA'
+  },
+  {
+    countryCode: 'RU',
+    regNr: '40444444444',
+    name: 'DDD SIA'
+  },
+  {
+    countryCode: 'EE',
+    regNr: '40999999999',
+    name: 'LAST SIA'
+  },
 ];
