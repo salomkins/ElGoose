1224db6fe10144d3be132be91b64295e3c058426
Guntars V <v.guntars@inbox.lv>
2018-04-19 13:00:00 +0300

    Sāku taisīt navBar priekš phone...

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,28 +1,41 @@
 import { Component, OnInit, HostListener, ElementRef } from '@angular/core';
 
+export class navItem {
+  name: string;
+  link: string;
+  subMenu?: {
+    name: string;
+    link: string;
+  }[];
+  subMenuActive?: boolean
+}
+
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
+
 export class NavbarComponent implements OnInit {
 
-  navItems: {
-    name: string,
-    link: string,
-    subMenu?: {
-      name: string,
-      link: string,
-    }[],
-    subMenuActive?: boolean,
-   }[];
+  navItems: navItem[];
+  activeItem: navItem;
   isStuck: boolean = false;
+  navBar: string;
 
   constructor(public el: ElementRef) { }
 
 
-subMenuOn(OnOff:boolean, item){
-  if(item.subMenu) item.subMenuActive = OnOff;
+  subMenuOn(OnOff: boolean, item: navItem) {
+    if (item.subMenu) item.subMenuActive = OnOff;
+  }
+
+navBarClick() {
+  if (this.navBar === 'fa-bars'){
+    this.navBar = 'fa-arrow-left';
+  }else{
+    this.navBar = 'fa-bars';
+  }
 }
 
   @HostListener('window:scroll', ['$event'])
@@ -36,14 +49,16 @@ subMenuOn(OnOff:boolean, item){
     this.navItems =
       [
         { name: "Home", link: "/home" },
-        { name: "About", link: "/about",
+        {
+          name: "About", link: "/about",
           subMenu: [
             { name: "Quick facts", link: "/about" },
             { name: "Who we are", link: "/about" },
             { name: "Our awards", link: "/about" },
           ]
         },
-        { name: "Services", link: "/services",
+        {
+          name: "Services", link: "/services",
           subMenu: [
             { name: "The best business services", link: "/services" },
             { name: "How we can help?", link: "/services" },
@@ -54,5 +69,7 @@ subMenuOn(OnOff:boolean, item){
         { name: "Contacts", link: "/contacts" },
         { name: "Test", link: "/test" },
       ];
+    this.activeItem = this.navItems[0];
+    this.navBar = 'fa-bars';
   }
 }
