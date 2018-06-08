50d8d8520469c05a1e5c57ae0e56ae8e37c98c7f
Janis Salmins <salomkins@gmail.com>
2018-06-08 13:46:42 +0300

    TD forms

--- src/app/components/content-contacts/content-contacts.module.ts
+++ src/app/components/content-contacts/content-contacts.module.ts
@@ -1,16 +1,17 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { ContentContactsRoutingModule } from './content-contacts-routing.module';
-import { ContentContactsComponent } from './content-contacts.component';
-import { MapComponent } from './map/map.component';
-import { FeedbackComponent } from './feedback/feedback.component';
-import { FooterContactsModule } from '../../components-lib/footer-contacts/footer-contacts.module';
-import { BtnModule } from '../../components-lib/btn/btn.module';
+import {ContentContactsRoutingModule} from './content-contacts-routing.module';
+import {ContentContactsComponent} from './content-contacts.component';
+import {MapComponent} from './map/map.component';
+import {FeedbackComponent} from './feedback/feedback.component';
+import {FooterContactsModule} from '../../components-lib/footer-contacts/footer-contacts.module';
+import {BtnModule} from '../../components-lib/btn/btn.module';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {FeedbackTDComponent} from './feedbackTD/feedbackTD.component';
 import {FeedbackMDComponent} from './feedbackMD/feedbackMD.component';
 import {FeedbackReactiveComponent} from './feedbackReactive/feedbackReactive.component';
+import {ForbiddenValidatorDirective} from './feedbackTD/forbidden-name.directive';
 
 @NgModule({
   imports: [
@@ -28,6 +29,8 @@ import {FeedbackReactiveComponent} from './feedbackReactive/feedbackReactive.com
     FeedbackTDComponent,
     FeedbackMDComponent,
     FeedbackReactiveComponent,
-  ]
+    ForbiddenValidatorDirective,
+  ],
 })
-export class ContentContactsModule { }
+export class ContentContactsModule {
+}
--- src/app/components/content-contacts/feedbackTD/feedbackTD.component.ts
+++ src/app/components/content-contacts/feedbackTD/feedbackTD.component.ts
@@ -8,10 +8,12 @@ import {Component, OnInit} from '@angular/core';
 export class FeedbackTDComponent implements OnInit {
   constructor() {
   }
-  public name = '';
-  public phone = '';
-  public email = '';
-  public message = '';
+
+  public name = 'Vasilij';
+  public phone = '29612349';
+  public email = 'vasilij.zhkov@gmail.com';
+  public message = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid architecto blanditiis debitis et id, nulla qui temporibus! Ad assumenda cupiditate doloremque eveniet expedita facilis inventore molestiae quae ratione? Quia, suscipit. ';
+
   ngOnInit() {
   }
 }
