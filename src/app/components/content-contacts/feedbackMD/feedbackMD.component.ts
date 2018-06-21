8ea1e78949a4598fde7e1f10870e6d08835ebab2
Janis Salmins <salomkins@gmail.com>
2018-06-21 20:44:20 +0300

    Uber validator

--- src/app/components/content-contacts/feedbackMD/feedbackMD.component.ts
+++ src/app/components/content-contacts/feedbackMD/feedbackMD.component.ts
@@ -1,8 +1,9 @@
-import {ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
+import {ChangeDetectionStrategy, Component, OnDestroy, OnInit} from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {filter, map, tap} from 'rxjs/operators';
 import {FeedbackType} from '../feedback/feedback.type';
 import {forbiddenNameValidator} from '../feedbackTD/forbidden-name.directive';
+import {Subscription} from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-feedback-md',
@@ -10,7 +11,7 @@ import {forbiddenNameValidator} from '../feedbackTD/forbidden-name.directive';
   styleUrls: ['./feedbackMD.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class FeedbackMDComponent implements OnInit {
+export class FeedbackMDComponent implements OnInit, OnDestroy {
   feedbackForm: FormGroup;
   phoneControl: FormControl = new FormControl('',
     [
@@ -21,8 +22,10 @@ export class FeedbackMDComponent implements OnInit {
   );
   submitDisabled = true;
   private _feedback: FeedbackType;
+  private phoneChangeSubscription: Subscription;
 
   constructor(private formBuilder: FormBuilder) {
+    this.handlePhoneChange = this.handlePhoneChange.bind(this);
   }
 
   ngOnInit() {
@@ -49,6 +52,7 @@ export class FeedbackMDComponent implements OnInit {
       ],
     });
 
+
     this.feedbackForm.valueChanges.pipe(
       filter(() => this.feedbackForm.valid),
       map(formData => {
@@ -66,6 +70,24 @@ export class FeedbackMDComponent implements OnInit {
     ).subscribe(subscribeData => {
       console.log('subscribeData', subscribeData);
     });
+
+
+    this.phoneChangeSubscription = this.phoneControl.valueChanges.pipe(
+      map(value => {
+        return value.replace(/[^0-9+() ]/g, '');
+      }),
+    ).subscribe(this.handlePhoneChange);
+    // this.phoneControl.setValue(this.phoneControl.value);
+  }
+
+  ngOnDestroy() {
+    this.phoneChangeSubscription.unsubscribe();
+  }
+
+  private handlePhoneChange(response: string) {
+    if (response !== this.phoneControl.value) {
+      this.phoneControl.setValue(response);
+    }
   }
 
   private get feedback(): FeedbackType {
